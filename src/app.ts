77c99dd4fed5869022809ae1388f0c2ff6a13ad9
Unknown <ty@gimpel.ca>
2021-07-09 09:09:57 -0700

    Initial TSConfig Setup

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, {Request, Response, Application, NextFunction} from 'express';
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello");
+});
+
+app.listen(5000, () => console.log("Server running"))
+console.log("Inital text!")
